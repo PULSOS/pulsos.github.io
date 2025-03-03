15fde70e443fb4af0fb8930e17f37dc2c65ab9e4
PULSOS - Encuentro Internacional de Artes Escénicas <pulsos.encuentro@gmail.com>
2025-03-03 04:47:55 -0400

    Cambios menores2

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ function toggleLanguage() {
     // Cambiar a inglés
     document.documentElement.lang = "en";
     document.getElementById("language-btn").textContent = "ESPAÑOL";
+    localStorage.setItem("language", "en"); // Guardar la preferencia en el localStorage
     switchContentLanguage("en");
   } else {
     // Cambiar a español
     document.documentElement.lang = "es";
     document.getElementById("language-btn").textContent = "ENGLISH";
+    localStorage.setItem("language", "es"); // Guardar la preferencia en el localStorage
     switchContentLanguage("es");
   }
 }
@@ -29,11 +31,16 @@ function switchContentLanguage(language) {
 
 // Detectar el idioma predeterminado al cargar la página y ajustar el botón
 document.addEventListener("DOMContentLoaded", function() {
-  let currentLanguage = document.documentElement.lang || "es"; // Predeterminado a español
+  let savedLanguage = localStorage.getItem("language");
+  let currentLanguage = savedLanguage || document.documentElement.lang || "es"; // Preferencia guardada o idioma predeterminado a español
+
   if (currentLanguage === "en") {
+    document.documentElement.lang = "en";
     document.getElementById("language-btn").textContent = "ESPAÑOL";
     switchContentLanguage("en");
   } else {
+    document.documentElement.lang = "es";
+    document.getElementById("language-btn").textContent = "ENGLISH";
     switchContentLanguage("es");
   }
 });
