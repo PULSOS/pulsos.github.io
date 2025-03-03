52d5555e61f54a053097c93b113d01d202d88485
PULSOS - Encuentro Internacional de Artes Escénicas <pulsos.encuentro@gmail.com>
2025-03-03 02:22:18 -0400

    Update

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function switchLanguage(language) {
-    const elements = document.querySelectorAll('[data-es], [data-en]');
-    elements.forEach(function(element) {
-      if (language === 'es') {
-        element.textContent = element.getAttribute('data-es');
-      } else {
-        element.textContent = element.getAttribute('data-en');
-      }
-    });
-  }
-  
+  const elements = document.querySelectorAll('[data-es], [data-en]');
+  elements.forEach(function(element) {
+    if (language === 'es') {
+      element.textContent = element.getAttribute('data-es');
+    } else {
+      element.textContent = element.getAttribute('data-en');
+    }
+  });
+}
