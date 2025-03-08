d61e499f13eee22610254093b4b584fb1d6b6b4b
PULSOS - Encuentro Internacional de Artes Escénicas <pulsos.encuentro@gmail.com>
2025-03-08 05:31:23 -0400

    menu lateral

--- script.js
+++ script.js
@@ -63,4 +63,15 @@ document.addEventListener("DOMContentLoaded", function () {
           }
       });
   }
+    // Asegurar que el menú lateral tenga las opciones correctas
+    const sidebarNav = sidebar.querySelector("nav");
+    if (sidebarNav) {
+        sidebarNav.innerHTML = `
+            <ul>
+                <li><a href="index.html" data-es="INICIO" data-en="HOME">INICIO</a></li>
+                <li><a href="edicion2025.html" data-es="EDICIÓN 2025" data-en="EDITION 2025">EDICIÓN 2025</a></li>
+                <li><a href="contacto.html" data-es="CONTACTO" data-en="CONTACT">CONTACTO</a></li>
+            </ul>
+        `;
+    }
 });
